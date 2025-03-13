efe9f778471ce8f0685d98b62d4094eab13c9ef9
Tonwilly <135318264+Tonwilly@users.noreply.github.com>
2025-03-13 17:51:03 +0300

    Update calculator.py
    Updating calculator file

--- calculator.py
+++ calculator.py
@@ -1,16 +1,20 @@
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
-def calculate() {
+def calculate():
+  add = 0
+  sub = 0
+  product = 0
+  quotient  = 0
   add = num1 + num2
   sub = num1 - num2
   product = num1 * num2
   quotient = num1 / num2
 
-print("Addition: " + num1 " + " + num2 " = " + add)
-print("Substraction: " + num1 + " - " + num2 + " = " + sub)
-print("Product: " + num1 + " * " + num2 + " = " + product)
-print("Division: " + num1 + " / " + num2 + " = " + quotient)
-}
+  print("Addition: " + str(num1) + " + " + str(num2) + " = " + str(add))
+  print("Substraction: " + str(num1) + " - " + str(num2) + " = " + str(sub))
+  print("Product: " + str(num1) + " * " + str(num2) + " = " + str(product))
+  print("Division: " + str(num1) + " / " + str(num2) + " = " + str(quotient))
+
 
 calculate()
