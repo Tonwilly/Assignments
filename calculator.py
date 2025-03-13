76de96fd15e6436b5b08cb72723d87bec45acea9
Tonwilly <135318264+Tonwilly@users.noreply.github.com>
2025-03-13 17:22:07 +0300

    Create simple calculator using python

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+def calculate() {
+  add = num1 + num2
+  sub = num1 - num2
+  product = num1 * num2
+  quotient = num1 / num2
+
+print("Addition: " + num1 " + " + num2 " = " + add)
+print("Substraction: " + num1 + " - " + num2 + " = " + sub)
+print("Product: " + num1 + " * " + num2 + " = " + product)
+print("Division: " + num1 + " / " + num2 + " = " + quotient)
+}
+
+calculate()
